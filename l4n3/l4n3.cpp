ce874fa6745294892387ed2d3e20c7a45199924f
Sanligod <Sanligod@yandex.ru>
2024-05-21 11:09:12 +0300

    Добавьте файлы проекта.

---
+++ l4n3/l4n3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+void printOddNumbers() {
+    int num;
+    std::cin >> num;
+
+    if (num == 0) {
+        return;
+    }
+
+    if (num % 2 != 0) {
+        std::cout << num << " ";
+    }
+
+    printOddNumbers();
+}
+
+int main() {
+    printOddNumbers();
+
+    return 0;
+}
